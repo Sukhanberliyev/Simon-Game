8001473ba0e8d2b068efe379e9298fe6a432f75d
Aidar Sukhanberliyev <68350747+Sukhanberliyev@users.noreply.github.com>
2020-11-13 21:29:26 +0900

    Added Audio / Animation

--- game.js
+++ game.js
@@ -1,29 +1,88 @@
-
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
+var userClickedPattern = [];
 
+var started = false;
 
-var userClickedPattern =[];
+var level = 0;
 
+$(".btn").click(function() {
 
-$(".btn").click(function(){
-  
   var userChosenColour = $(this).attr("id");
-
   userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
 
 });
 
+function nextSequence() {
 
+  userClickedPattern = [];
 
-function nextSequence(){
+  level++;
+
+  $("#level-title").text("lever" + level);
+
   var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColour = buttonColours[randomNumber];s
-
+  var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
+
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
+}
+
+//animation
+
+function animatePress(currentColour) {
+
+  $("#" + currentColour).addClass("pressed");
+
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+//Keyboard pressed
+
+$(document).keyPress(function() {
+  if(!started){
+
+    $("#level-title").text("lever" + level);
+    nextSequence();
+    started = true;
+
+  }
+})
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log("success");
+
+      if (userClickedPattern.length === gamePattern.length){
+
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+
+      }
+
+    } else {
+
+      console.log("wrong");
+
+    }
+
 }
